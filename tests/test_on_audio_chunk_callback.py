7527e82d85b04622d757ebf43ecdbfc8c3365758
Kolja Beigel <kolja.beigel@web.de>
2024-02-21 19:38:54 +0100

    v0.3.42

--- tests/test_on_audio_chunk_callback.py
+++ tests/test_on_audio_chunk_callback.py
@@ -15,28 +15,3 @@ while stream.is_playing():
     time.sleep(0.1)
 
 engine.shutdown()
-
-
-
-# if __name__ == '__main__':
-#     from RealtimeTTS import TextToAudioStream, CoquiEngine
-#     import time
-
-
-#     def dummy_generator():
-#         yield "我喜欢读书。天气很好。我们去公园吧。今天是星期五。早上好。这是我的朋友。请帮我。吃饭了吗？我在学中文。晚安。"
-
-
-#     def on_audio_chunk_callback(chunk):
-#         print(f"Chunk received, len: {len(chunk)}")
-
-
-#     engine = CoquiEngine(language="zh")
-#     stream = TextToAudioStream(engine).feed(dummy_generator())
-#     stream.play_async(tokenizer="stanza", language="zh", on_audio_chunk=on_audio_chunk_callback, muted=True)
-#     #stream.play_async(tokenizer="stanza", language="zh")
-
-#     while stream.is_playing():
-#         time.sleep(0.1)
-
-#     engine.shutdown()
