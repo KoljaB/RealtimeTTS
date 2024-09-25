b6eafef8ace6cdef53755ace35bd67c29f7ce70b
Raj Hammeer Singh Hada <hammeerraj@gmail.com>
2024-09-25 14:14:55 +0530

    - openai tts docker pushed

---
+++ docker/openai-tts-docker/app/main.py
@@ -0,0 +1,179 @@
+if __name__ == '__main__':
+    print("Starting server")
+    import logging
+
+    # Enable or disable debug logging
+    DEBUG_LOGGING = False
+
+    if DEBUG_LOGGING:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+
+
+from RealtimeTTS import (
+    TextToAudioStream,
+    OpenAIEngine
+)
+
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Query, Request
+
+from queue import Queue
+import threading
+import logging
+import uvicorn
+import wave
+import io
+import os
+
+PORT = int(os.environ.get("TTS_FASTAPI_PORT", 8000))
+WORKERS = int(os.environ.get("WORKERS", 4))
+
+BROWSER_IDENTIFIERS = [
+    "mozilla",
+    "chrome",
+    "safari",
+    "firefox",
+    "edge",
+    "opera",
+    "msie",
+    "trident"
+]
+
+origins = [
+    "http://localhost",
+    f"http://localhost:{PORT}",
+    "http://127.0.0.1",
+    f"http://127.0.0.1:{PORT}",
+    "https://localhost",
+    f"https://localhost:{PORT}",
+    "https://127.0.0.1",
+    f"https://127.0.0.1:{PORT}",
+]
+
+play_text_to_speech_semaphore = threading.Semaphore(1)
+speaking_lock = threading.Lock()
+tts_lock = threading.Lock()
+gen_lock = threading.Lock()
+
+class TTSRequestHandler:
+    def __init__(self, engine):
+        self.engine = engine
+        self.audio_queue = Queue()
+        self.stream = TextToAudioStream(engine, on_audio_stream_stop=self.on_audio_stream_stop, muted=True)
+        self.speaking = False
+
+    def on_audio_chunk(self, chunk):
+        self.audio_queue.put(chunk)
+    
+    def on_audio_stream_stop(self):
+        self.audio_queue.put(None)
+        self.speaking = False
+
+    def play_text_to_speech(self, text):
+        self.speaking = True
+        self.stream.feed(text)
+        logging.debug(f"Playing audio for text: {text}")
+        print(f"Synthesizing: \"{text}\"")
+        self.stream.play_async(on_audio_chunk=self.on_audio_chunk, muted=True)            
+
+    def audio_chunk_generator(self, send_wave_headers):
+        first_chunk = False
+        try:
+            while True:
+                chunk = self.audio_queue.get()
+                if chunk is None:
+                    print("Terminating stream")
+                    break
+                if not first_chunk:
+                    if send_wave_headers:
+                        print("Sending wave header")
+                        yield create_wave_header_for_engine(self.engine)
+                    first_chunk = True
+                yield chunk
+        except Exception as e:
+            print(f"Error during streaming: {str(e)}")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Define a CSP that allows 'self' for script sources for firefox
+csp = {
+    "default-src": "'self'",
+    "script-src": "'self'",
+    "style-src": "'self' 'unsafe-inline'",
+    "img-src": "'self' data:",
+    "font-src": "'self' data:",
+    "media-src": "'self' blob:",
+}
+csp_string = "; ".join(f"{key} {value}" for key, value in csp.items())
+
+
+@app.middleware("http")
+async def add_security_headers(request: Request, call_next):
+    response = await call_next(request)
+    response.headers['Content-Security-Policy'] = csp_string
+    return response
+
+def is_browser_request(request):
+    user_agent = request.headers.get('user-agent', '').lower()
+    is_browser = any(
+        browser_id in user_agent for browser_id in BROWSER_IDENTIFIERS)
+    return is_browser
+
+def create_wave_header_for_engine(engine):
+    _, _, sample_rate = engine.get_stream_info()
+
+    num_channels = 1
+    sample_width = 2
+    frame_rate = sample_rate
+
+    wav_header = io.BytesIO()
+    with wave.open(wav_header, 'wb') as wav_file:
+        wav_file.setnchannels(num_channels)
+        wav_file.setsampwidth(sample_width)
+        wav_file.setframerate(frame_rate)
+
+    wav_header.seek(0)
+    wave_header_bytes = wav_header.read()
+    wav_header.close()
+
+    # Create a new BytesIO with the correct MIME type for Firefox
+    final_wave_header = io.BytesIO()
+    final_wave_header.write(wave_header_bytes)
+    final_wave_header.seek(0)
+
+    return final_wave_header.getvalue()
+
+@app.get("/tts")
+async def tts(request: Request, text: str = Query(...)):
+    with tts_lock:
+        request_handler = TTSRequestHandler(OpenAIEngine())
+        browser_request = is_browser_request(request)
+
+        if play_text_to_speech_semaphore.acquire(blocking=False):
+            try:
+                threading.Thread(
+                    target=request_handler.play_text_to_speech,
+                    args=(text,),
+                    daemon=True).start()
+            finally:
+                play_text_to_speech_semaphore.release()
+
+        return StreamingResponse(
+            request_handler.audio_chunk_generator(browser_request),
+            media_type="audio/wav"
+        )
+
+if __name__ == '__main__':
+    print("Initializing TTS Engines")
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT, workers=WORKERS)
