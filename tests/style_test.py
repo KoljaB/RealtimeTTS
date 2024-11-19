e9ba4a4e95c628aa46e31962adc786de627cc9e8
KoljaB <kolja.beigel@web.de>
2024-11-19 16:35:46 +0100

    styletts (experimental)

---
+++ tests/style_test.py
@@ -0,0 +1,21 @@
+if __name__ == "__main__":
+    from RealtimeTTS import TextToAudioStream, StyleTTSEngine
+
+    def dummy_generator():
+        yield "Hey guys! These here are realtime spoken sentences based on local text synthesis. "
+        yield "With a local, neuronal, cloned voice. So every spoken sentence sounds unique."
+
+    # for normal use with minimal logging:
+    engine = StyleTTSEngine()
+
+    # test with extended logging:
+    # import logging
+    # logging.basicConfig(level=logging.INFO)
+    # engine = CoquiEngine(level=logging.INFO)
+
+    stream = TextToAudioStream(engine)
+
+    print("Starting to play stream")
+    stream.feed(dummy_generator()).play(log_synthesized_text=True)
+
+    engine.shutdown()
