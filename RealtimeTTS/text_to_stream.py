6cd50c4600c3db207b94571dda111776b18debcd
EzShrimps <ezshrimps@gmail.com>
2025-06-16 18:44:34 -0700

    print SYNTHESIS FINISHED only when log_synthesized_text is on

--- RealtimeTTS/text_to_stream.py
+++ RealtimeTTS/text_to_stream.py
@@ -557,7 +557,9 @@ class TextToAudioStream:
                                 print(f"Traceback: {tb_str}")
                                 print(f"Error: {e}")
 
-                            print("SYNTHESIS FINISHED")
+                            if log_synthesized_text:
+                                print(f"\033[92m\033[1m✔ SYNTHESIS FINISHED\033[0m")
+                            
                             if not synthesis_successful:
                                 if len(self.engines) == 1:
                                     time.sleep(0.2)
