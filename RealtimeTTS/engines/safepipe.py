9c96c8b9655f067e55e65d78124596e2b2f30f51
Kolja Beigel <kolja.beigel@web.de>
2026-05-31 19:07:22 +0200

    Avoid configuring root logging in safepipe

--- RealtimeTTS/engines/safepipe.py
+++ RealtimeTTS/engines/safepipe.py
@@ -5,9 +5,6 @@ import threading
 import time
 import logging
 
-# Configure logging. Adjust level and formatting as needed.
-logging.basicConfig(level=logging.DEBUG,
-                    format='[%(asctime)s] %(levelname)s:%(name)s: %(message)s')
 logger = logging.getLogger(__name__)
 
 try:
